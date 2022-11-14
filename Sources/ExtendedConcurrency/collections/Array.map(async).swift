1664872ef16cb3dd4132a4947aa37033e3140e0b
Leopold Lemmermann <leopold@lemmermann.com>
2022-11-14 13:23:49 +0100

    Added some array methods for async mapping.

---
+++ Sources/ExtendedConcurrency/collections/Array.map(async).swift
@@ -0,0 +1,44 @@
+//	Created by Leopold Lemmermann on 11.11.22.
+
+public extension Array {
+  /// Asynchronously maps the array and returns the result.
+  /// - Parameter transform: The transformation to apply to the array's elements.
+  /// - Returns: An array of the transformed values.
+  func map<T>(_ transform: (Element) async throws -> T) async rethrows -> [T] {
+    var values = [T]()
+    for element in self {
+      values.append(try await transform(element))
+    }
+    return values
+  }
+  
+  /// Asynchronously maps the array and streams the result (available when transform is non-throwing).
+  /// - Parameter transform: The transformation to apply to the array's elements.
+  /// - Returns: An AsyncStream of the transformed values.
+  func map<T>(_ transform: @escaping (Element) async -> T) -> AsyncStream<T> {
+    AsyncStream { continuation in
+      for element in self {
+        continuation.yield(await transform(element))
+      }
+
+      continuation.finish()
+    }
+  }
+  
+  /// Asynchronously maps the array and streams the result (available when transform is throwing).
+  /// - Parameter transform: The transformation to apply to the array's elements.
+  /// - Returns: An AsyncThrowingStream of the transformed values and errors.
+  func map<T>(_ transform: @escaping (Element) async throws -> T) -> AsyncThrowingStream<T, Error> {
+    AsyncThrowingStream { continuation in
+      do {
+        for element in self {
+          continuation.yield(try await transform(element))
+        }
+
+        continuation.finish()
+      } catch {
+        continuation.finish(throwing: error)
+      }
+    }
+  }
+}
