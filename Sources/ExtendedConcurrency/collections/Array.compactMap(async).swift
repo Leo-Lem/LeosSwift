779166f60331086b6db277074adc56707d5bc474
Leopold Lemmermann <leopold@lemmermann.com>
2022-11-14 15:30:43 +0100

    Made the array map overloads disfavored.

--- Sources/ExtendedConcurrency/collections/Array.compactMap(async).swift
+++ Sources/ExtendedConcurrency/collections/Array.compactMap(async).swift
@@ -4,6 +4,7 @@ public extension Array {
   /// Asynchronously maps the array, while dropping nil results, and returns the result.
   /// - Parameter transform: The transformation to apply to the array's elements.
   /// - Returns: An array of the transformed values.
+  @_disfavoredOverload
   func compactMap<T>(_ transform: (Element) async throws -> T?) async rethrows -> [T] {
     var values = [T]()
     for element in self {
@@ -15,6 +16,7 @@ public extension Array {
   /// Asynchronously maps the array, while dropping nil results, and streams the result (available when transform is non-throwing).
   /// - Parameter transform: The transformation to apply to the array's elements.
   /// - Returns: An AsyncStream of the transformed values.
+  @_disfavoredOverload
   func compactMap<T>(_ transform: @escaping (Element) async -> T?) -> AsyncStream<T> {
     AsyncStream { continuation in
       for element in self {
@@ -28,6 +30,7 @@ public extension Array {
   /// Asynchronously maps the array, while dropping nil results, and streams the result (available when transform is throwing).
   /// - Parameter transform: The transformation to apply to the array's elements.
   /// - Returns: An AsyncThrowingStream of the transformed values and errors.
+  @_disfavoredOverload
   func compactMap<T>(_ transform: @escaping (Element) async throws -> T?) -> AsyncThrowingStream<T, Error> {
     AsyncThrowingStream { continuation in
       do {
