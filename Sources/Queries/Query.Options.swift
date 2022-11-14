8d3fe0b2a82d21609649b4342d312b56c6aab25e
Leopold Lemmermann <leopold@lemmermann.com>
2022-11-14 14:14:26 +0100

    Added an option for specifying batch size for asynchronous stream fetching

--- Sources/Queries/Query.Options.swift
+++ Sources/Queries/Query.Options.swift
@@ -2,12 +2,19 @@
 
 public extension Query {
   struct Options {
+    
+    /// The maximum number of items to be fetched
     public let maxItems: Int?
+    
+    /// The number of items to be fetched at a time (when fetching asynchronously)
+    public let batchSize: Int
 
     public init(
-      maxItems: Int? = nil
+      maxItems: Int? = nil,
+      batchSize: Int = 1
     ) {
       self.maxItems = maxItems
+      self.batchSize = batchSize
     }
   }
 }
