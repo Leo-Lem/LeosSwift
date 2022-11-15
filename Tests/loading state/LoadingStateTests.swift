670900625751e22be26d941cedbfe75a9266759a
Leopold Lemmermann <leopold@lemmermann.com>
2022-11-15 22:57:02 +0100

    Added unit tests for LoadingState.

---
+++ Tests/loading state/LoadingStateTests.swift
@@ -0,0 +1,53 @@
+//	Created by Leopold Lemmermann on 15.11.22.
+
+@testable import Concurrency
+import XCTest
+
+final class LoadingStateTests: XCTestCase {
+  func testAddingElementStartsLoading() {
+    var state = LoadingState<String>.idle
+
+    let element = "Hello"
+    state.add(element)
+
+    XCTAssertEqual(state, .loading([element]), "Loading State is not loading with the correct elements.")
+  }
+
+  func testSettingLoadedRetainsElements() {
+    var state = LoadingState<String>.idle
+
+    let element = "Hello"
+    state.add(element)
+
+    state.finish()
+
+    XCTAssertEqual(state, .loaded([element]), "Loading State is not loaded with the correct elements.")
+  }
+
+  func testUpdatingElementsOverridesOldElements() {
+    var state = LoadingState<String>.idle
+
+    let element1 = "Hello"
+    let element2 = "Goodbye"
+    state.add(element1)
+    state.update(element2)
+    state.finish()
+
+    XCTAssertEqual(state, .loaded([element2]), "Loading State is not loaded with the updated elements.")
+  }
+
+  func testFailingKeepsCorrectError() {
+    var state = LoadingState<String>.idle
+
+    let error = ExampleError.sumTingWong
+    state.finish {
+      throw error
+    }
+
+    if case let .failed(otherError) = state {
+      XCTAssertEqual(error, otherError as? ExampleError, "Loading State does not hold specific error.")
+    } else {
+      XCTFail("Loading State is not failed.")
+    }
+  }
+}
