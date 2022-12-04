143ac42d7a837144c0edc2298afaf24d7b54c328
Leopold Lemmermann <leopold@lemmermann.com>
2022-12-04 20:45:49 +0100

    Made the stream receive on runloop main.

--- Sources/streams/Publisher.stream.swift
+++ Sources/streams/Publisher.stream.swift
@@ -1,12 +1,13 @@
 //	Created by Leopold Lemmermann on 02.12.22.
 
 import protocol Combine.Publisher
+import class Foundation.RunLoop
 
 @available(iOS 15, macOS 12, *)
 public extension Publisher where Failure == Never {
   var stream: AsyncStream<Output> {
     AsyncStream { continuation in
-      for await element in values { continuation.yield(element) }
+      for await element in receive(on: RunLoop.main).values { continuation.yield(element) }
       continuation.finish()
     }
   }
@@ -17,7 +18,7 @@ public extension Publisher where Failure == Error {
   var stream: AsyncThrowingStream<Output, Error> {
     AsyncThrowingStream { continuation in
       do {
-        for try await element in values { continuation.yield(element) }
+        for try await element in receive(on: RunLoop.main).values { continuation.yield(element) }
         continuation.finish()
       } catch { continuation.finish(throwing: error) }
     }
